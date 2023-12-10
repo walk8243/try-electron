73e9f21804f2dcb1bcc48beecfb69fa3158fe842
walk8243 <walk8243@gmail.com>
2023-12-10 22:42:57 +0900

    Issueフレームの調整

--- renderer/components/Issue.tsx
+++ renderer/components/Issue.tsx
@@ -1,10 +1,19 @@
 import issueStyles from '../styles/Issue.module.scss'
+import headlineStyles from '../styles/Headline.module.scss'
 
 const Issue = ({ url }: { url?: string }) => (
   <section className={issueStyles.box}>
-    <h3 style={{ display: 'none' }}>Issue</h3>
-    <p>{url ?? ' '}</p>
+    <h3 className={headlineStyles['header--hidden']}>Issue</h3>
+    <IssueUrlBar url={url ?? ' '} />
+    <div></div>
   </section>
+)
+
+const IssueUrlBar = ({ url }: { url: string }) => (
+  <div>
+    <h4 className={headlineStyles['header--hidden']}>Issue URLバー</h4>
+    <p>{url}</p>
+  </div>
 )
 
 export default Issue
