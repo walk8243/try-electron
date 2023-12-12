b73355161859df89b015b8abfbf350c1b15b0685
walk8243 <walk8243@gmail.com>
2023-12-13 00:44:19 +0900

    取得するissueが多すぎないようにする

--- electron-src/utils/github.ts
+++ electron-src/utils/github.ts
@@ -1,13 +1,27 @@
 import { net, safeStorage } from 'electron'
 import log from 'electron-log/main'
+import dayjs from 'dayjs'
 import { store } from './store'
 
 export const getUserInfo = async () => {
 	return await accessGithub({ path: 'user' })
 }
 
-export const getIssues = async (page: number = 1) => {
-	return await accessGithub({ path: 'issues', query: { filter: 'all', state: 'all', sort: 'updated', per_page: 100, page } })
+export const getIssues = async () => {
+	return Promise.all([
+		accessGithub({ path: 'issues', query: { filter: 'assigned', state: 'all', sort: 'updated', per_page: 100, page: 1 } }),
+		accessGithub({ path: 'issues', query: { filter: 'created', state: 'all', sort: 'updated', per_page: 100, page: 1 } }),
+		accessGithub({ path: 'issues', query: { filter: 'mentioned', state: 'all', sort: 'updated', per_page: 100, page: 1 } }),
+		accessGithub({ path: 'issues', query: { filter: 'subscribed', state: 'all', sort: 'updated', per_page: 100, page: 1 } }),
+	]).then((values) => {
+		return values.reduce((acc, cur) => acc.concat(cur), [])
+			.reduce((acc: Record<string, any>[], cur: Record<string, any>) => {
+				if (acc.every((issue) => issue.node_id !== cur.node_id)) {
+					acc.push(cur)
+				}
+				return acc
+			}, [])
+	}).then((issues) => issues.sort((a, b) => dayjs(a.updated_at).isBefore(dayjs(b.updated_at)) ? 1 : -1))
 }
 
 export const checkStoreData = () => {
