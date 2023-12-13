2367c2991f5a40489ad804d20a5646d00db4c520
walk8243 <walk8243@gmail.com>
2023-12-14 00:25:08 +0900

    @material/webを使ってみる

--- renderer/components/IssueList.tsx
+++ renderer/components/IssueList.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useState, Dispatch, SetStateAction, MouseEvent } from 'react'
-import { GithubIssue } from '../interfaces/Github'
+import { createElement, useEffect, useState } from 'react'
+import type { ReactNode, Dispatch, SetStateAction, MouseEvent } from 'react'
+import type { GithubIssue } from '../interfaces/Github'
 
+import '@material/web/list/list'
+import '@material/web/list/list-item'
 import issueListStyles from '../styles/IssueList.module.scss'
 import headlineStyles from '../styles/Headline.module.scss'
 
 type Props = {
   issueUrlHandler: Dispatch<SetStateAction<string>>
 }
+const numberFormat = Intl.NumberFormat('ja-JP')
 let issueTimer: NodeJS.Timeout;
 
 const IssueList = ({ issueUrlHandler }: Props) => {
@@ -23,7 +27,7 @@ const IssueList = ({ issueUrlHandler }: Props) => {
     }, 5 * 60 * 1000);
   }, [])
 
-  const handleClick = (e: MouseEvent<HTMLAnchorElement>, url: string) => {
+  const handleClick = (e: MouseEvent, url: string) => {
     issueUrlHandler(() => url)
     window.electron?.issue(url)
     e.preventDefault()
@@ -32,15 +36,36 @@ const IssueList = ({ issueUrlHandler }: Props) => {
   return (
     <section className={issueListStyles.box}>
       <h3 className={headlineStyles['header--hidden']}>Issueリスト</h3>
-      <ul>
+      <div>
+        <p>Issues</p>
+        <p>{numberFormat.format(issues.length)} issues</p>
+      </div>
+      <ListBox>
         {issues.map((issue) => (
-          <li key={issue.node_id}>
-            <a href={issue.html_url} className={`${issueListStyles.link} ${issue.state === 'closed' ? issueListStyles['link--visited'] : ''}`} onClick={(e) => handleClick(e, issue.html_url)}>{issue.title}</a>
-          </li>
+          <Issue key={issue.node_id} issue={issue} handle={handleClick} />
         ))}
-      </ul>
+      </ListBox>
     </section>
   )
 }
+
+const ListBox = ({ children }: { children: ReactNode }) => (
+  createElement('md-list', {}, children)
+)
+
+const Issue = ({ issue, handle }: { issue: GithubIssue, handle: (e: MouseEvent, url: string) => void }) => (
+  createElement('md-list-item', {
+    style: { color: issue.state === 'open' ? '#111' : '#444', backgroundColor: issue.state === 'open' ? '#eee' : '#aaa', cursor: 'pointer' },
+    onClick: (e) => handle(e, issue.html_url)
+  }, (
+    <p className={issueListStyles['issue_title']} style={{ color: issue.state === 'open' ? '#3fb950' : '#a371f7' }}>
+      {Object.hasOwn(issue, 'pull_request') ? 'PR' : 'Issue'}: {issue.state}
+    </p>
+  ), (
+    <p>{issue.title}</p>
+  ), (
+    <p style={{ fontSize: 'small' }}>{issue.repository.full_name}</p>
+  ))
+)
 
 export default IssueList
