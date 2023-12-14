37d95f69fb7543c7c7e079e64fcf0f129e02a74f
walk8243 <walk8243@gmail.com>
2023-12-15 01:05:04 +0900

    IndexPageのDOM構成を変更

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,7 +1,8 @@
-import { useReducer, useState, Reducer } from 'react'
+import { useReducer, useState, Reducer, ReactNode } from 'react'
 import Head from 'next/head'
 
 import { IssueFilterContext, IssueFilterDispatchContext, issueFilterAll, IssueFilter } from '../context/IssueFilterContext'
+import { Heading } from '../components/Heading'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Menu from '../components/Menu'
@@ -9,28 +10,42 @@ import IssueList from '../components/IssueList'
 import Issue from '../components/Issue'
 
 import styles from '../styles/index.module.scss'
-import headlineStyles from '../styles/Headline.module.scss'
 
-const IndexPage = () => {
-  const [ issueUrl, setIssueUrl ] = useState('')
-  const [ issueFilter, dispatch ] = useReducer<Reducer<IssueFilter, IssueFilter>>((_prevFilter, currentfilter) => currentfilter, issueFilterAll)
+const IndexPage = () => (
+  <>
+    <Head>
+      <title>Amethyst</title>
+    </Head>
+    <IssueFilterContextProvider>
+      <MainComponent />
+    </IssueFilterContextProvider>
+  </>
+)
+
+const MainComponent = () => {
+  const [issueUrl, setIssueUrl] = useState('')
+
+  return (
+    <div className={styles.box}>
+      <Header />
+      <main className={styles.main}>
+        <Heading level={2} hidden={true}>メイン</Heading>
+        <Menu />
+        <IssueList issueUrlHandler={setIssueUrl} />
+        <Issue url={issueUrl} />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+const IssueFilterContextProvider = ({ children }: { children: ReactNode }) => {
+  const [issueFilter, dispatch] = useReducer<Reducer<IssueFilter, IssueFilter>>((_prevFilter, currentfilter) => currentfilter, issueFilterAll)
 
   return (
     <IssueFilterContext.Provider value={issueFilter}>
       <IssueFilterDispatchContext.Provider value={dispatch}>
-        <div className={styles.box}>
-          <Head>
-            <title>Amethyst</title>
-          </Head>
-          <Header />
-          <main className={styles.main}>
-            <h2 className={headlineStyles['header--hidden']}>メイン</h2>
-            <Menu />
-            <IssueList issueUrlHandler={setIssueUrl} />
-            <Issue url={issueUrl} />
-          </main>
-          <Footer />
-        </div>
+        {children}
       </IssueFilterDispatchContext.Provider>
     </IssueFilterContext.Provider>
   )
