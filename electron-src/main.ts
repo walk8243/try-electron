fa581e39410b2a037b3e809f87bc7badc3cccb09
walk8243 <walk8243@gmail.com>
2023-12-22 00:16:43 +0900

    main関数を切り出す

--- electron-src/main.ts
+++ electron-src/main.ts
@@ -1,5 +1,14 @@
 import { join } from 'node:path';
-import { app, ipcMain, Menu, Notification, session, shell } from 'electron';
+import {
+	app,
+	BrowserView,
+	BrowserWindow,
+	ipcMain,
+	Menu,
+	Notification,
+	session,
+	shell,
+} from 'electron';
 import prepareNext from 'electron-next';
 import isDev from 'electron-is-dev';
 import log from 'electron-log/main';
@@ -23,87 +32,16 @@ const isMac = process.platform === 'darwin';
 let latestIssueGainTime: dayjs.Dayjs | null = null;
 
 export const main = async () => {
-	const mainWindow = windowUtils.createMain();
-	ipcMain.handle('app:version', () => `v${app.getVersion()}`);
-	ipcMain.on('app:ready', (_event) => {
-		log.verbose('App renderer is ready');
-
-		if (store.has('userInfo')) {
-			mainWindow.webContents.send('app:pushUser', store.get('userInfo'));
-		}
-		if (store.has('issueData')) {
-			mainWindow.webContents.send(
-				'app:pushIssues',
-				store.get('issueData')?.issues ?? [],
-			);
-			mainWindow.webContents.send(
-				'app:pushUpdatedAt',
-				store.get('issueData')?.updatedAt ?? '',
-			);
-		}
-
-		store.onDidChange('userInfo', (userInfo) => {
-			mainWindow.webContents.send('app:pushUser', userInfo ?? {});
-		});
-		store.onDidChange('issueData', (data) => {
-			log.debug('蓄積しているデータが更新されました');
-			if (!data) return;
-			mainWindow.webContents.send('app:pushUpdatedAt', data.updatedAt ?? {});
-			mainWindow.webContents.send('app:pushIssues', data.issues ?? []);
-		});
-	});
+	const mainWindow = setupMainWindow();
 
 	await prepareNext('./renderer');
 	mainWindow.loadURL(getLoadedUrl());
 	gainGithubUser().catch((_err) => {});
 	gainGithubIssues().catch((_err) => {});
 
-	const webview = windowUtils.createWebview();
-	mainWindow.setBrowserView(webview);
-	windowUtils.putWebview(mainWindow, webview);
-
-	const settingWindow = windowUtils.createSetting(mainWindow);
-	const aboutWindow = windowUtils.createAbout(mainWindow);
-	const menu = createMenu({
-		parentWindow: mainWindow,
-		webview,
-		settingWindow,
-		aboutWindow,
-	});
-	if (isMac) {
-		Menu.setApplicationMenu(menu);
-	} else {
-		mainWindow.setMenu(menu);
-	}
-	mainWindow.show();
-
-	ipcMain.handle('github:issue', async (_event, url: string) => {
-		webview.webContents.loadURL(url);
-	});
-	ipcMain.on('browser:open', (_event, url: string) => {
-		shell.openExternal(url);
-	});
-
-	if (!checkStoreData()) {
-		settingWindow.show();
-	}
-
-	mainWindow
-		.on('maximize', () => {
-			windowUtils.putWebview(mainWindow, webview);
-		})
-		.on('unmaximize', () => {
-			windowUtils.putWebview(mainWindow, webview);
-		})
-		.on('resized', () => {
-			windowUtils.putWebview(mainWindow, webview);
-		})
-		.on('enter-full-screen', () => {
-			windowUtils.putWebview(mainWindow, webview, { noHeaderFlag: true });
-		})
-		.on('leave-full-screen', () => {
-			windowUtils.putWebview(mainWindow, webview);
-		});
+	const webview = setupWebview(mainWindow);
+	setupModalWindow(mainWindow, webview);
+	setupResizedSetting(mainWindow, webview);
 
 	setInterval(
 		() => {
@@ -114,12 +52,7 @@ export const main = async () => {
 		5 * 60 * 1000,
 	);
 
-	if (isDev) {
-		await installExtension(REACT_DEVELOPER_TOOLS);
-		await session.defaultSession.loadExtension(
-			join(app.getPath('userData'), 'extensions', REACT_DEVELOPER_TOOLS.id),
-		);
-	}
+	await setupDevtools();
 };
 
 export const gainGithubUser = async () => {
@@ -150,4 +83,99 @@ export const gainGithubIssues = async () => {
 
 	store.set('issueData', { updatedAt: now.toISOString(), issues });
 	return issues;
+};
+
+const setupMainWindow = () => {
+	const mainWindow = windowUtils.createMain();
+	ipcMain.handle('app:version', () => `v${app.getVersion()}`);
+	ipcMain.on('app:ready', (_event) => {
+		log.verbose('App renderer is ready');
+
+		if (store.has('userInfo')) {
+			mainWindow.webContents.send('app:pushUser', store.get('userInfo'));
+		}
+		if (store.has('issueData')) {
+			mainWindow.webContents.send(
+				'app:pushIssues',
+				store.get('issueData')?.issues ?? [],
+			);
+			mainWindow.webContents.send(
+				'app:pushUpdatedAt',
+				store.get('issueData')?.updatedAt ?? '',
+			);
+		}
+
+		store.onDidChange('userInfo', (userInfo) => {
+			mainWindow.webContents.send('app:pushUser', userInfo ?? {});
+		});
+		store.onDidChange('issueData', (data) => {
+			log.debug('蓄積しているデータが更新されました');
+			if (!data) return;
+			mainWindow.webContents.send('app:pushUpdatedAt', data.updatedAt ?? {});
+			mainWindow.webContents.send('app:pushIssues', data.issues ?? []);
+		});
+	});
+
+	return mainWindow;
+};
+const setupWebview = (mainWindow: BrowserWindow) => {
+	const webview = windowUtils.createWebview();
+	mainWindow.setBrowserView(webview);
+	windowUtils.putWebview(mainWindow, webview);
+	ipcMain.handle('github:issue', async (_event, url: string) => {
+		webview.webContents.loadURL(url);
+	});
+	ipcMain.on('browser:open', (_event, url: string) => {
+		shell.openExternal(url);
+	});
+	return webview;
+};
+const setupModalWindow = (mainWindow: BrowserWindow, webview: BrowserView) => {
+	const settingWindow = windowUtils.createSetting(mainWindow);
+	const aboutWindow = windowUtils.createAbout(mainWindow);
+	const menu = createMenu({
+		parentWindow: mainWindow,
+		webview,
+		settingWindow,
+		aboutWindow,
+	});
+	if (isMac) {
+		Menu.setApplicationMenu(menu);
+	} else {
+		mainWindow.setMenu(menu);
+	}
+	mainWindow.show();
+
+	if (!checkStoreData()) {
+		settingWindow.show();
+	}
+};
+const setupResizedSetting = (
+	mainWindow: BrowserWindow,
+	webview: BrowserView,
+) => {
+	mainWindow
+		.on('maximize', () => {
+			windowUtils.putWebview(mainWindow, webview);
+		})
+		.on('unmaximize', () => {
+			windowUtils.putWebview(mainWindow, webview);
+		})
+		.on('resized', () => {
+			windowUtils.putWebview(mainWindow, webview);
+		})
+		.on('enter-full-screen', () => {
+			windowUtils.putWebview(mainWindow, webview, { noHeaderFlag: true });
+		})
+		.on('leave-full-screen', () => {
+			windowUtils.putWebview(mainWindow, webview);
+		});
+};
+const setupDevtools = async () => {
+	if (isDev) {
+		await installExtension(REACT_DEVELOPER_TOOLS);
+		await session.defaultSession.loadExtension(
+			join(app.getPath('userData'), 'extensions', REACT_DEVELOPER_TOOLS.id),
+		);
+	}
 };
