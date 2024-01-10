a82b6369b2067681d8df63650e10c3e9a39b8cb4
walk8243 <walk8243@gmail.com>
2024-01-11 00:55:25 +0900

    本番ではinfoログ以上を出力する

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,9 +1,14 @@
 import { app } from 'electron';
+import isDev from 'electron-is-dev';
 import log from 'electron-log/main';
 import { main } from './main';
 
 log.initialize({ preload: true });
 log.eventLogger.startLogging({});
+if (isDev) {
+	log.transports.file.level = 'info';
+	log.transports.console.level = 'info';
+}
 
 app.on('ready', () => {
 	log.verbose('App is ready');
