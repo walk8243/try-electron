8a700582935b84375c43b044edea53145dfc7012
walk8243 <walk8243@gmail.com>
2024-01-14 00:41:01 +0900

    エラー時の画面表示を調整

--- renderer/components/ErrorBoundary.tsx
+++ renderer/components/ErrorBoundary.tsx
@@ -1,20 +1,22 @@
 import log from 'electron-log';
 import { Component } from 'react';
-import { Typography } from '@mui/material';
-import PropTypes from 'prop-types';
+import { Box, Grid, Link, Typography } from '@mui/material';
+
+const ISSUE_URL =
+	'https://github.com/walk8243/amethyst-electron/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=';
 
 export class ErrorBoundary extends Component<
 	{ children: React.ReactNode },
-	{ hasError: boolean },
+	{ hasError: boolean; error: Error | null },
 	unknown
 > {
-	state = { hasError: false };
-	static propTypes = {
-		children: PropTypes.node,
-	};
+	constructor(props: { children: React.ReactNode }) {
+		super(props);
+		this.state = { hasError: false, error: null };
+	}
 
-	static getDerivedStateFromError(_error: unknown) {
-		return { hasError: true };
+	static getDerivedStateFromError(error: Error) {
+		return { hasError: true, error };
 	}
 
 	componentDidCatch(error: Error, _errorInfo: unknown) {
@@ -24,7 +26,33 @@ export class ErrorBoundary extends Component<
 
 	render() {
 		if (this.state.hasError) {
-			return <Typography variant="h1">エラーが発生しました。</Typography>;
+			return (
+				<Grid
+					container
+					width="100vw"
+					maxWidth="600px"
+					height="100%"
+					justifyContent="center"
+					sx={{ overflow: 'hidden' }}
+				>
+					<Grid item width="100%">
+						<Typography variant="h1">エラーが発生しました。</Typography>
+						<Typography variant="subtitle1">
+							<Link href={ISSUE_URL}>GitHubのIssue</Link>
+							までご報告をお願い致します
+						</Typography>
+						<Box m={2}>
+							{this.state.error ? (
+								<Typography>
+									{this.state.error.name}: {this.state.error.message}
+								</Typography>
+							) : (
+								<></>
+							)}
+						</Box>
+					</Grid>
+				</Grid>
+			);
 		}
 
 		return this.props.children;
