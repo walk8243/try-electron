51a1304d2a2be9b4be5cfc0ebe268cdff2f5c3ac
のぶ <walk8243@gmail.com>
2024-02-01 13:12:03 +0900

    RateLimitのための設定の見直し (#87)
    * 不要なカスタム設定を削除
    
    * 制限期間内の処理回数を上げる
    
    * electronのバージョンアップ

--- electron-src/utils/queue.ts
+++ electron-src/utils/queue.ts
@@ -1,20 +1,3 @@
-import PriorityQueue, {
-	type PriorityQueueOptions,
-} from 'p-queue/dist/priority-queue';
-import { RunFunction } from 'p-queue/dist/queue';
-
 export const QUEUE_CONCURRENCY = 2; // 並列処理数
 export const QUEUE_INTERVAL = 60 * 1000; // 制約期間
-export const QUEUE_INTERVAL_CAP = 20; // 制約期間内の処理回数
-export const QUEUE_SIZE_LIMIT = 100; // キューの最大サイズ
-
-export class CustomQueueClass extends PriorityQueue {
-	enqueue(run: RunFunction, options?: Partial<PriorityQueueOptions>) {
-		if (this.size >= QUEUE_SIZE_LIMIT) {
-			if (!options || !options.priority || options.priority < 1) {
-				return;
-			}
-		}
-		super.enqueue(run, options);
-	}
-}
+export const QUEUE_INTERVAL_CAP = 100; // 制約期間内の処理回数
