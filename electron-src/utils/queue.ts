fb5b5d94f43a6056530ef21ff6541cfb3bb0981c
walk8243 <walk8243@gmail.com>
2024-02-01 13:04:21 +0900

    制限期間内の処理回数を上げる

--- electron-src/utils/queue.ts
+++ electron-src/utils/queue.ts
@@ -1,4 +1,3 @@
 export const QUEUE_CONCURRENCY = 2; // 並列処理数
 export const QUEUE_INTERVAL = 60 * 1000; // 制約期間
-export const QUEUE_INTERVAL_CAP = 20; // 制約期間内の処理回数
-export const QUEUE_SIZE_LIMIT = 100; // キューの最大サイズ
+export const QUEUE_INTERVAL_CAP = 100; // 制約期間内の処理回数
